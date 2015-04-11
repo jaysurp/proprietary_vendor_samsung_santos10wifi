46756d8ebfebc986c151f7d3d5686c3f0afd5a7b
Benoît Laniel <nels+git@pgroupe.net>
2015-04-11 10:39:49 +0200

    build.prop hacks for Google Play Store

--- device-vendor.mk
+++ device-vendor.mk
@@ -3,6 +3,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.dalvik.vm.isa.arm=x86 \
     ro.enable.native.bridge.exec=1
 
+# Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=santos10wifixx \
+    PRIVATE_BUILD_DESC="santos10wifixx-user 4.4.2 KOT49H P5210XXUBNG4 release-keys" \
+    BUILD_FINGERPRINT="samsung/santos10wifixx/santos10wifi:4.4.2/KOT49H/P5210XXUBNG4:user/release-keys"
+
+# Google Play Store compatibility
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+
 # OMX packages
 PRODUCT_PACKAGES += \
     libstagefrighthw \
